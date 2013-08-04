c0a75710ad2d9f17270d0f1278bd149cdcec4709
An Nguyen Hoang <hoangan2701@gmail.com>
2013-08-04 08:31:49 +0700

    V1.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WKS.WKS_SERVICES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
